42c9c4ce2c47d6115944f3ac319b2c84b6b572c1
TrishaBalaji <31964190+TrishaBalaji@users.noreply.github.com>
2026-03-06 16:46:52 -0500

    Resolved module not found error

--- alt_view.js
+++ alt_view.js
@@ -5,7 +5,7 @@ const mustacheExpress = require('mustache-express');
 
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
-app.set('views', _dirname + '/views');
+app.set('views', __dirname + '/views');
 
 app.get('/mypage',function(req, res) {
 
@@ -19,11 +19,12 @@ app.get('/mypage',function(req, res) {
     }
 
     res.render('mypage', TPL); //module not found error given when running server 
+    //error resolved - had to install mustache-express and correct the typo in dirname (two underscores not one)
 })
 
 app.get(/^(.+)$/, function(req, res){
     console.log("static file request: " + req.params[0]);
-    res.sendFile(_dirname + req.params[0]);
+    res.sendFile(__dirname + req.params[0]);
 });
 
 var server = app.listen(8081, function()
