97c42f5d8b9a496ff4553163b266655513f99410
TrishaBalaji <31964190+TrishaBalaji@users.noreply.github.com>
2026-03-21 09:42:15 -0400

    Create form.js to add form validation for "How much should I stock?" function

---
+++ form.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() { 
+const sold = document.getElementById('bags_sold')
+const ship = document.getElementById('bags_ship')
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (sold.value === '' || sold.value == null) {
+    messages.push('Bags sold is required')
+  }
+
+  if (ship.value === '' || ship.value == null) {
+    messages.push('Bags shipped is required')
+  }
+
+  if (sold.value.length <= 0) {
+    messages.push('Bags sold must be more than 0')
+  } 
+
+  if (ship.value.length <= 0) {
+    messages.push('Bags shipped must be more than 0')
+  }
+
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+}) 
+});
